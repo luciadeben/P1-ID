b039343ed6b56fb45c4ede577cc5dc09fcb0b033
luciadeben <91801656+luciadeben@users.noreply.github.com>
2023-10-30 17:22:40 +0100

    Residente.java

---
+++ TuteladoJPA-1.1-src/TuteladoJPA-1.1/src/main/java/gei/id/tutelado/model/Residente.java
@@ -0,0 +1,82 @@
+package gei.id.tutelado.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import gei.id.tutelado.model.Habitacion;
+
+@NamedQueries ({
+        @NamedQuery (name="Residente.recuperaPorNif",
+                query="SELECT r FROM Residente r where r.nif=:nif"),
+        @NamedQuery (name="Residente.recuperaTodos",
+                query="SELECT r FROM Residente e ORDER BY r.nif")
+})
+
+@Entity
+@Table(name="Residente_tcc")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Residente extends Persona {
+
+    @Column(nullable = false, unique = false)
+    private date fecha_ingreso;
+
+    @Column(nullable = false, unique=false)
+    private String estadoSalud;
+
+    @Column(nullable = false, unique=false)
+    private List<String> contactosEmergencia;
+
+    @Column(nullable = false, unique=false)
+    private Habitacion habitacion;
+
+
+    public date getFechaIngreso() {
+        return fecha_ingreso;
+    }
+
+
+    public void setFechaIngreso(date fecha_ingreso) {
+        this.fecha_ingreso = fecha_ingreso;
+    }
+
+
+    public String getEstadosalud() {
+        return estadoSalud;
+    }
+
+
+    public void setEstadosalud(String estadoSalud) {
+        this.estadoSalud = estadoSalud;
+    }
+
+
+    public List<String> getContactosEmergencia() {
+        return contactosEmergencia;
+    }
+
+
+    public void setContactosEmergencia(List<String> contactosEmergencia) {
+        this.contactosEmergencia = contactosEmergencia;
+    }
+
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Residente [fecha_ingreso=" + fecha_ingreso + ", estadoSalud=" + estadoSalud + ", contactosEmergencia=" + contactosEmergencia + ", habitacion=" + habitacion + "]";
+    }
+
+
+
+
+
+
+}
