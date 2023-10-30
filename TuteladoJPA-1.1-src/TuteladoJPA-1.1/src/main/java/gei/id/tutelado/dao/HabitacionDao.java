f87997fe41c338fd7b2d704d75b57fa439b9bcfd
luciadeben <91801656+luciadeben@users.noreply.github.com>
2023-10-30 17:14:40 +0100

    HabitacionDao.java

---
+++ TuteladoJPA-1.1-src/TuteladoJPA-1.1/src/main/java/gei/id/tutelado/dao/HabitacionDao.java
@@ -0,0 +1,21 @@
+package gei.id.tutelado.dao;
+
+import java.util.List;
+
+import gei.id.tutelado.configuracion.Configuracion;
+import gei.id.tutelado.model.Habitacion;
+
+public interface HabitacionDao {
+
+    void setup (Configuracion config);
+
+    // OPERACIONS CRUD BASICAS
+    Habitacion almacena (Habitacion habitacion);
+    Habitacion modifica (Habitacion habitacion);
+    void elimina (Habitacion habitacion);
+    Habitacion recuperaPorNumero (int numero);
+
+    //QUERIES ADICIONAIS
+    List<Habitacion> recuperaTodos();
+}
+
