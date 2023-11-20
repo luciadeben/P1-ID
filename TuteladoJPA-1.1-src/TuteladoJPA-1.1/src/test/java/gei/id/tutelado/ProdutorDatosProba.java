581197b52ec7b137f7fad2130fc1e5f02a6bcc79
maria-sestelo <maria.sestelo@udc.es>
2023-11-20 18:21:00 +0100

    Update ProdutorDatosProba.java
    Test funcionan ok

--- TuteladoJPA-1.1-src/TuteladoJPA-1.1/src/test/java/gei/id/tutelado/ProdutorDatosProba.java
+++ TuteladoJPA-1.1-src/TuteladoJPA-1.1/src/test/java/gei/id/tutelado/ProdutorDatosProba.java
@@ -120,7 +120,6 @@ public class ProdutorDatosProba {
 
 		// Crea dos residentes EN MEMORIA: r0, r1
 		creaHabitaciones();
-		gravaHabitaciones();
 		
 		this.r0 = new Residente();
         this.r0.setNif("010V");
@@ -136,7 +135,6 @@ public class ProdutorDatosProba {
 		listaContactos();
 		this.r0.setContactosEmergencia(contactos);
 		this.r0.setHabitacion(h0);
-
 
 		this.r1 = new Residente ();
         this.r1.setNif("044C");
@@ -162,8 +160,6 @@ public class ProdutorDatosProba {
 	public void creaHabitaciones() {
 
 		// Crea dos habitaciones EN MEMORIA: h0, h1
-		creaEmpleados();
-		gravaEmpleados();
 
 		this.h0 = new Habitacion();
 		this.h0.setNumero(2);
